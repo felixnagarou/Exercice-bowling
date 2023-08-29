c8e56cdc58c9e66bfc2fa27803bf4cd469664f0c
Hippolyte <hippolyteaubin@gmail.com>
2023-08-29 15:01:07 +0200

    first commit

---
+++ src/main/java/org/example/bowling/Frame.java
@@ -0,0 +1,11 @@
+package org.example.bowling;
+
+public class Frame {
+
+    private int numberOfKeelHit; //todo Mockito pour générer un coup aléatoire
+
+    private int keelNumber;
+    private int score;
+
+
+}
